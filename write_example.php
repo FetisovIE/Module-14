28a1a9db81c8a3b5ab657f9b9f40cd4c7855fb35
Fetisov Igor <ig.fetisoff@gmail.com>
2022-06-16 18:51:08 +0300

    some bug fixed

--- write_example.php
+++ write_example.php
@@ -4,7 +4,7 @@ $testArray = [1, 2, 3, 5, 9, 8];
 
 $anotherArray = [0, 0, 0, 0, 0];
 
-$fp = fopen
+$fp = fopen ('example.txt', 'a+');
 
 file_put_contents('example.txt', $testArray);
 file_put_contents('example.txt', $anotherArray, FILE_APPEND);
